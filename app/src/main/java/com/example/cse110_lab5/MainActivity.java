f5601592e8d4df7f371da2c71862128270b9cb5d
Tuan (Tony) Tran <tmt003@ucsd.edu>
2022-05-01 21:01:08 -0700

    Design Changes
    + Added a Plan button
    + Reformat the ordered list

--- app/src/main/java/com/example/cse110_lab5/MainActivity.java
+++ app/src/main/java/com/example/cse110_lab5/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, GraphActivity.class);
-        startActivity(intent);
+        Intent notEmpty = new Intent(this, GraphActivity.class);
+
+        final Button button = findViewById(R.id.plan_bttn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                startActivity(notEmpty);
+            }
+        });
+
     }
 }
