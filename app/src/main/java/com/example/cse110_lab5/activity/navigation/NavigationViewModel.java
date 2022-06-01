8d342bf9ed585a3a2ec4ede18d85ff576ff168b3
Aaron Yu <aaronyu330@gmail.com>
2022-05-31 17:53:16 -0700

    Remove references to entrance_exit_gate (#94)

--- app/src/main/java/com/example/cse110_lab5/activity/navigation/NavigationViewModel.java
+++ app/src/main/java/com/example/cse110_lab5/activity/navigation/NavigationViewModel.java
@@ -47,7 +47,7 @@ public class NavigationViewModel extends AndroidViewModel {
         this.edgeDao = db.edgeDao();
         this.nodeDao = db.nodeDao();
 
-        ZooData.Node start = nodeDao.get("entrance_exit_gate");
+        ZooData.Node start = nodeDao.getGate();
         this.lastKnownCoord = new Coord(start.lat, start.lng);
     }
 
@@ -123,7 +123,7 @@ public class NavigationViewModel extends AndroidViewModel {
                 remainingExhibits
                         .subList(0, remainingExhibits.size() - 1) // Exclude returning to the start
                         .toArray(new String[remainingExhibits.size() - 1]),
-                "entrance_exit_gate"));
+                nodeDao.getGate().id));
 
         Log.d("Navigation/Closest Exhibit", closestExhibit);
         Log.d("Navigation/Remaining Exhibits", remainingExhibits.toString());
